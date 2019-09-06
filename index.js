e52009c9b91084c992da6d4e190af2e11f519336
Bruno <brunosimashadlich@gmail.com>
2019-09-06 14:46:59 -0300

    Adicionado respostas para mensagens sobre java e docker

--- index.js
+++ index.js
@@ -16,36 +16,94 @@ http.createServer(function (req, res) {
 	res.end("hit");
 }).listen(process.env.PORT || 5000)
 
+function tech_comparison(text) {
+	//TODO check if it's DM or channel			
+	//split the > using positive lookahead to avoid citation match
+	var split = (text).split(/(?!^)(&gt\;|&lt\;)/);
+
+	//is it a treta???
+	if (split.length > 1) {
+		var response;
+		//generate a random number to get some chance to do some extra actinos
+		var randomNumber = Math.floor((Math.random()*100) + 1);
+		if(randomNumber < 10) {
+			response = "Isso mesmo! " + text;
+		} else {
+			//Reverse the comparison
+			//Some chance of appending some text
+			if(randomNumber > 80) {
+				response = "Calma aí, não é bem assim... é: " + split.reverse().join('');
+			} else {
+				response = split.reverse().join('');
+			}
+		}
+		return response;
+	}
+	return "";
+}
+
+function java_performance(text) {
+	text = text.toLowerCase();
+
+	options = ["Java e performance são palavras que não combinam!",
+		   "Vai la escrever um Hello World de 10 linhas vai!",
+		   "Fala isso pro garbage collector taokey.",
+		   "Quando sair a especificação do Java 30 podemos conversar.",
+		   "Jávai tarde"
+	]
+
+	if (text.indexOf("java") > -1 && text.indexOf("performance") > -1) {
+		option = Math.floor(Math.random() * options.length)
+		if (option == options.length) {
+			option--;
+		}
+		return options[option];
+	}
+
+	return "";
+}
+
+function docker(text) {
+	text = text.toLowerCase();
+
+	options = ["Alguém disse https://www.youtube.com/watch?v=BkWJik6QjeQ ?",
+		   "Por que ao invés de usar docker você não vai para baleia? https://www.youtube.com/watch?v=BkWJik6QjeQ"
+	]
+
+	if (text.indexOf("docker") > -1) {
+		option = Math.floor(Math.random() * options.length)
+		if (option == options.length) {
+			option--;
+		}
+		return options[option];
+	}
+	return "";
+}
+
+function process_msg(message) {
+	text = message.text;
+
+	if ((response = tech_comparison(text)).length > 0 ||
+	    (response = java_performance(text)).length > 0 ||
+	    (response = docker(text)).length > 0) {
+		return response;
+	}
+
+	return "";
+}
+
 //listen to messages and plant the treta
 rtm.on(RTM_EVENTS.MESSAGE, function handleRtmMessage(message) {
 	//check if it's a message
 	if (message.type == "message" && message.text != undefined) {
-		//TODO check if it's DM or channel			
-		//split the > using positive lookahead to avoid citation match
-		var split = (message.text).split(/(?!^)(&gt\;|&lt\;)/);
-
-		//is it a treta???
-		if (split.length > 1) {
-			var response;
-			//generate a random number to get some chance to do some extra actinos
-			var randomNumber = Math.floor((Math.random()*100) + 1);
-			if(randomNumber < 10) {
-				response = "Isso mesmo! " + message.text;
-			} else {
-				//Reverse the comparison
-				//Some chance of appending some text
-				if(randomNumber > 80) {
-					response = "Calma aí, não é bem assim... é: " + split.reverse().join('');
-				} else {
-					response = split.reverse().join('');
-				}
-			}
-			//do it!
+		response = process_msg(message);
+		if (response.length > 0) {
 			rtm.sendMessage(response, message.channel);
 			//mission accomplished...
 			console.log("the treta has been planted: " + response);
 		} else {
 			//it's not a treta
+			console.log(message.text);
 			console.log("just silly talk...");
 		}
 	}
